a63c3c1a13ab96bf1ac9f419a92eda1a82ab1620
George Otniel <georgeotniel5@gmail.com>
2022-09-24 11:34:42 +0700

    update

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ $('body').animate({
     scrollTop: elemenTujuan.offset().top - 50
 }, 1250, 'easeInOutExpo'); 
 }); 
+
+// Paralax
+// about
+$(window).on('load', function(){
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul'); 
+});   
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+    });
+
+    // portfolio 
+    if(wScroll > $('.portfolio').offset().top - 250){
+        $('.portfolio .thumbnail').each(function(i){
+            setTimeout(function(){
+                $('.portfolio .thumbnail').eg(i).addClass('Muncul');
+            }, 300 * (i+1));       
+        });
+    }
+});
